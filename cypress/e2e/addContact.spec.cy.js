4ab78d7a5d2fb882327a806ee6fe185b8bee2312
Brian Haucke <brian@recroom.com>
2024-09-27 12:57:36 -0700

    First real cypress test

---
+++ cypress/e2e/addContact.spec.cy.js
@@ -0,0 +1,22 @@
+describe('Add Contact Tests', () => {
+    it('Can add a new contact', () => {
+      cy.visit('https://thinking-tester-contact-list.herokuapp.com/')
+      cy.get('#email').type('brian@test.com')
+      cy.get('#password').type('1234567')
+      cy.get('#submit').click()
+      cy.get('#add-contact').click()
+      cy.get('#firstName').type('Test')
+      cy.get('#lastName').type('User')
+      cy.get('#birthdate').type('2000-01-02')
+      cy.get('#email').type('foo@bar.com')
+      cy.get('#phone').type('8005551212')
+      cy.get('#street1').type('123 Fake Str.')
+      cy.get('#street2').type('Apt. 222')
+      cy.get('#city').type('Covington')
+      cy.get('#stateProvince').type('WA')
+      cy.get('#postalCode').type('98042')
+      cy.get('#country').type('USA')
+      cy.get('#submit').click()
+
+    })
+  })
