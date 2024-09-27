ba6bc5b597b648521d790d1861e1489dad2e521c
Brian Haucke <brian@recroom.com>
2024-09-27 16:48:04 -0700

    Update test with Assertions to validate our data

--- cypress/e2e/addContact.spec.cy.js
+++ cypress/e2e/addContact.spec.cy.js
@@ -1,11 +1,14 @@
 describe('Add Contact Tests', () => {
     it('Can add a new contact', () => {
+
+      let random = Math.floor(Math.random() * 1000)
+
       cy.visit('https://thinking-tester-contact-list.herokuapp.com/')
       cy.get('#email').type('brian@test.com')
       cy.get('#password').type('1234567')
       cy.get('#submit').click()
       cy.get('#add-contact').click()
-      cy.get('#firstName').type('Test')
+      cy.get('#firstName').type('Test' + random)
       cy.get('#lastName').type('User')
       cy.get('#birthdate').type('2000-01-02')
       cy.get('#email').type('foo@bar.com')
@@ -17,6 +20,21 @@ describe('Add Contact Tests', () => {
       cy.get('#postalCode').type('98042')
       cy.get('#country').type('USA')
       cy.get('#submit').click()
+
+      cy.contains('Test' + random + ' User').click()
+      cy.get('#firstName').should('contain', `Test${random}`)
+      cy.get('#lastName').should('contain', 'User')
+      cy.get('#birthdate').should('contain', '2000-01-02')
+      cy.get('#email').should('contain', 'foo@bar.com')
+      cy.get('#phone').should('contain', '8005551212')
+      cy.get('#street1').should('contain', '123 Fake Str.')
+      cy.get('#street2').should('contain', 'Apt. 222')
+      cy.get('#city').should('contain', 'Covington')
+      cy.get('#stateProvince').should('contain', 'WA')
+      cy.get('#postalCode').should('contain', '98042')
+      cy.get('#country').should('contain', 'USA')
+      //cy.get('#test').should('contain', 'This is a test. Comment Me Out!')
+      
 
     })
   })
