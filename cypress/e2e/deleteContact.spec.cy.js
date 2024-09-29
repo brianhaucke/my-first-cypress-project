fb296639bb88d1b15a87e31828e892f46eccbc74
Brian Haucke <brian@recroom.com>
2024-09-28 20:38:34 -0700

    modify add contact, create delete contact test

--- cypress/e2e/deleteContact.spec.cy.js
+++ cypress/e2e/deleteContact.spec.cy.js
@@ -1,5 +1,5 @@
-describe('Add Contact Tests', () => {
-    it('Can add a new contact', () => {
+describe('Delete Contact Tests', () => {
+    it('Can Delete a contact', () => {
 
       let random = Math.floor(Math.random() * 1000)
 
@@ -22,20 +22,9 @@ describe('Add Contact Tests', () => {
       cy.get('#submit').click()
 
       cy.contains('Test' + random + ' User').click()
-      cy.get('#firstName').should('contain', `Test${random}`)
-      cy.get('#lastName').should('contain', 'User')
-      cy.get('#birthdate').should('contain', '2000-01-02')
-      cy.get('#email').should('contain', 'foo@bar.com')
-      cy.get('#phone').should('contain', '8005551212')
-      cy.get('#street1').should('contain', '123 Fake Str.')
-      cy.get('#street2').should('contain', 'Apt. 222')
-      cy.get('#city').should('contain', 'Covington')
-      cy.get('#stateProvince').should('contain', 'WA')
-      cy.get('#postalCode').should('contain', '98042')
-      cy.get('#country').should('contain', 'USA')
-
       cy.get('#delete').click()
       cy.on('window:confirm', () => true)
+      cy.get('.contactTableBodyRow').contains('Test' + random + ' User').should('not.exist')
       
       
 
